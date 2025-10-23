8418b7a050f3f7a474c403b501370bc06ca2d6a7
Michael Hoyt <hoytnix@users.noreply.github.com>
2025-10-23 16:25:05 -0400

    Delete product.py

--- product.py
+++
@@ -1,8 +0,0 @@
-class Product:
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-    def display_product(self):
-        return f"Product: {self.name}, Price: ${self.price}, Quantity: {self.quantity}"
